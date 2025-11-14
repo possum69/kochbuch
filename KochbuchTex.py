ae9ab26ee4d78f1e4b2b5e3b5810d9560bbc5c48
possum69 <bernd.mattern69@gmail.com>
2025-11-14 17:25:59 +0100

    Din A4 update

--- KochbuchTex.py
+++ KochbuchTex.py
@@ -5,6 +5,7 @@ import json
 from Kochbuch import Kochbuch
 
 OUT_PATH = "Kochbuch.tex"
+PICTURES_PATH = "Bilder"
 
 class KochbuchTex(Kochbuch):
     def __init__(self):
@@ -13,6 +14,7 @@ class KochbuchTex(Kochbuch):
     def latex_escape(s: str) -> str:
         if not isinstance(s, str):
             s = str(s)
+        
         # minimal escaping for common LaTeX special chars
         return re.sub(r'([#\$%&\~_\^\{\\\}])', r'\\\1', s)
 
@@ -38,14 +40,24 @@ class KochbuchTex(Kochbuch):
                 with open(prefix, "r", encoding="utf-8") as pf:
                     f.write(pf.read() + "\n")
 
-            kapitel = []
+            # read existing chapters
+            try:
+                with open("Kapitel.json", "r", encoding="utf-8") as kap_file:
+                    kapitel = json.load(kap_file)
+            except Exception:
+                kapitel = []
+
+            # collect chapters from documents
             for doc in self.kochbuch["documents"]:
                 kap = doc.get("Kapitel", "Lost and Found")
                 if kap not in kapitel:
                     kapitel.append(kap)
 
-            # loop capturse
             count = 0
+            with open("Kapitel.json", "w", encoding="utf-8") as kap_file:
+                json.dump(kapitel, kap_file, indent=4, ensure_ascii=False)
+
+            # loop capturse
             for kap in kapitel:
                 kap_title = kap
                 print("Writing chapter: ", kap_title)
@@ -87,11 +99,12 @@ class KochbuchTex(Kochbuch):
                         f.write("\\end{directions}\n")
 
                     if doc.get("Bild"):
-                        image_path = doc.get("Bild")
-                        if os.path.isfile(f"Quellen/{image_path}"):
+                        image_file = doc.get("Bild")
+                        file_path = f"{PICTURES_PATH}/{image_file}"
+                        if os.path.isfile(file_path):
                             f.write("\\begin{figure}[h]\n")
                             f.write("\\centering\n")
-                            f.write(f"\\includegraphics[width=0.5\\textwidth]{{{KochbuchTex.latex_escape(image_path)}}}\n")
+                            f.write(f"\\includegraphics[width=0.75\\textwidth]{{{KochbuchTex.latex_escape(file_path)}}}\n")
                             f.write(f"\\caption{{{title_tex}}}\n")
                             f.write("\\end{figure}\n")
 
@@ -112,7 +125,7 @@ def main():
     os.system(f"pdflatex -quiet {OUT_PATH}")
     os.system(f"makeindex Kochbuch.idx")
     os.system(f"pdflatex -quiet {OUT_PATH}")
-    os.system("del *.aux *.log *.idx *.ind *.toc *.out *.ilg")
+    os.system("del *.aux *.log *.idx *.ind *.toc *.out *.ilg *.lof")
     os.system("move Kochbuch.pdf Output/Kochbuch.pdf")
 
 if __name__ == "__main__":
